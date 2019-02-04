1343fd3d8aaad50f7ebd9b4b21bfd297e1ba6e6f
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2019-02-04 13:04:34 -0200

    Changes in the API

--- goeievraag/api.py
+++ goeievraag/api.py
@@ -1,15 +1,13 @@
 import json
-import re
 
 from flask import Flask, request
 from main import GoeieVraag
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'datasets/'
 
 model = GoeieVraag()
 
-@app.route("/search/<query>", methods=['GET'])
+@app.route("/rank/<query>", methods=['GET'])
 def search(query=''):
     '''
     :return: return the 10 most semantic-similar questions to the query based on our official sysmte
@@ -18,21 +16,6 @@ def search(query=''):
 
     if request.method == 'GET' and query.strip() != '':
         questions = model(query.strip())
-        questions = { 'code':200, 'result': [q[1] for q in questions] }
-
-    return json.dumps(questions)
-
-@app.route("/rank/<query>", methods=['GET'])
-def rank(query):
-    '''
-    return the 10 most semantic-similar questions to the query based on our baseline (bm25)
-    :param query:
-    :return:
-    '''
-    questions = {'code': 400}
-
-    if request.method == 'GET' and query.strip() != '':
-        questions = model.retrieve(query.strip(), n=10)
-        questions = { 'code':200, 'result': [q[1] for q in questions] }
+        questions = { 'code':200, 'result': questions }
 
     return json.dumps(questions)
